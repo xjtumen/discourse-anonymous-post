2c2d0137f8f5556354ec4a32eda707c9ba9a1145
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2023-08-05 21:49:44 +0800

    cannot work

--- javascripts/discourse/api-initializers/discord-widget.js
+++ javascripts/discourse/api-initializers/discord-widget.js
@@ -1,54 +1,90 @@
-import { apiInitializer } from "discourse/lib/api";
+import {apiInitializer} from "discourse/lib/api";
 import User from "discourse/models/user";
 
 export default apiInitializer("0.11.1", (api) => {
-  // If login is required
-  if (settings.require_login && !api.getCurrentUser()) {
-    return;
-  }
-
-  // If a trust level is required
-  if (User.currentProp("trust_level") < settings.minimum_trust_level) {
-    return;
-  }
-
-  // If user must be staff
-  if (settings.require_staff && !api.getCurrentUser().staff) {
-    return;
-  }
-
-  // If user must be a group member
-  if (settings.required_groups.length > 0) {
-    const requiredGroups = settings.required_groups
-      .split("|")
-      .map((g) => Number(g));
-
-    const currentUserGroups = api.getCurrentUser().groups.map((g) => g.id);
-
-    if (!currentUserGroups.some((g) => requiredGroups.includes(g))) {
-      return;
+    // If login is required
+    if (settings.require_login && !api.getCurrentUser()) {
+        return;
     }
-  }
 
-  api.decorateWidget("header-icons:before", (helper) => {
-    const headerState = helper.widget.parentWidget.state;
-    return helper.attach("header-dropdown", {
-      title: themePrefix("anonymouspost_widget.title"),
-      icon: "reply", // original fab-discord
-      active: headerState.anonymouspostChatVisible,
-      action: "toggleanonymouspost",
-      classNames: ["anonymouspost-button"],
+    // If a trust level is required
+    if (User.currentProp("trust_level") < settings.minimum_trust_level) {
+        return;
+    }
+
+    // If user must be staff
+    if (settings.require_staff && !api.getCurrentUser().staff) {
+        return;
+    }
+
+    // If user must be a group member
+    if (settings.required_groups.length > 0) {
+        const requiredGroups = settings.required_groups
+            .split("|")
+            .map((g) => Number(g));
+
+        const currentUserGroups = api.getCurrentUser().groups.map((g) => g.id);
+
+        if (!currentUserGroups.some((g) => requiredGroups.includes(g))) {
+            return;
+        }
+    }
+
+    let res = window.location.href.match(/\/t\/(.*?)\/(\w+)/);
+    if (res && res[2] > 0) {
+    } else {
+        return;
+    }
+
+    api.decorateWidget("header-icons:before", (helper) => {
+        const headerState = helper.widget.parentWidget.state;
+        return helper.attach("header-dropdown", {
+            title: themePrefix("anonymouspost_widget.title"),
+            icon: "reply", // original fab-discord
+            active: headerState.anonymouspostChatVisible,
+            action: "toggleanonymouspost",
+            classNames: ["anonymouspost-button"],
+        });
     });
-  });
 
-  api.decorateWidget("header-icons:after", (helper) => {
-    const headerState = helper.widget.parentWidget.state;
-    if (headerState.anonymouspostChatVisible) {
-      return [helper.attach("anonymouspost-chat-menu")];
+    api.decorateWidget("header-icons:after", (helper) => {
+        const headerState = helper.widget.parentWidget.state;
+        if (headerState.anonymouspostChatVisible) {
+            return [helper.attach("anonymouspost-chat-menu")];
+        }
+    });
+
+    api.attachWidgetAction("header", "toggleanonymouspost", function () {
+        this.state.anonymouspostChatVisible = !this.state.anonymouspostChatVisible;
+    });
+    const toggleAnonReplyToTopic = function() {
+        this.sendWidgetAction("toggleanonymouspost");
+        return res
+    };
+
+    const currentLocale = I18n.currentLocale();
+    if (!I18n.translations[currentLocale].js.composer) {
+        I18n.translations[currentLocale].js.composer = {};
+        I18n.translations["en"].js.composer = {};
+        I18n.translations["zh_CN"].js.composer = {};
     }
-  });
+    I18n.translations["en"].js.anon_reply_to_topic_icon_text = "Reply Anonymously";
+    I18n.translations["zh_CN"].js.anon_reply_to_topic_icon_text = "匿名回复该话题";
+    I18n.translations[currentLocale].js.anon_reply_to_topic_icon_text = "Anonymously Reply";
+    let a = function (e) {
+        console.log(e);
+        return e.applySurround('<div data-theme="foo">\n\n', "\n\n</div>", "my_button_text");
+    }
+    api.registerTopicFooterButton({
+        id: "replytotopic-button",
+        icon: "reply",
+        title: "Reply Anonymously",
+        label: "anon_reply_to_topic_icon_text",
+        action(context) {
+            a
+        },
 
-  api.attachWidgetAction("header", "toggleanonymouspost", function () {
-    this.state.anonymouspostChatVisible = !this.state.anonymouspostChatVisible;
-  });
+        // action: this.toggleAnonReplyToTopic
+    })
+
 });
--- javascripts/discourse/widgets/discord-chat-menu.js
+++ javascripts/discourse/widgets/discord-chat-menu.js
@@ -8,35 +8,23 @@ createWidget("anonymouspost-chat-menu", {
     tagName: "div.anonymouspost-panel",
 
     html() {
-        // const replyURLbase = `https://anonymouspost.xjtu.live/xjtumen-custom-api/handle-reply-to-topic/`;
         // const replyURLbase = `http://127.0.0.1:7010/xjtumen-custom-api/handle-reply-to-topic/`;
         const replyURLbase = 'https://' + window.location.hostname + '/xjtumen-custom-api/handle-reply-to-topic/';
 
-        let replyURL;
-        let allowReply = false;
-        // allowReply = this.siteSettings.requ`ire_login;
-
-        const register = this.register;
-        const router = register.lookup("router:main");
+        let replyURL = replyURLbase;
 
         try {
-            var res = router.currentURL.match(/\/t\/(.*?)\/(\w+)/);
+            var res = window.location.href.match(/\/t\/(.*?)\/(\w+)/);
             if (res && res[2] > 0) {
-                replyURL = replyURLbase + window.location.hostname + "/" + res[2] + "/" + document.title
-            } else {
-                allowReply = false;
-                replyURL = replyURLbase;
+                replyURL = replyURLbase + window.location.hostname + "/" + res[2] + "/" + document.title;
             }
         } catch (e) {
             replyURL = replyURLbase;
         }
-        // if (!allowReply) {
-        //     return
-        // }
 
-        if (this.site.mobileView) {
-            return DiscourseURL.routeTo(replyURL);
-        }
+        // if (this.site.mobileView) {
+        //     return DiscourseURL.routeTo(replyURL);
+        // }
 
         return this.attach("menu-panel", {
             contents: () =>
